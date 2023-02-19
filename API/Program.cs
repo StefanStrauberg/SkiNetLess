b35be509036be7f68373c4c224b5ffbdc372bc8d
StefanStrauberg <stefanstrauberg@gmail.com>
2023-02-19 20:13:30 +0300

    Configure StoreContext

--- API/Program.cs
+++ API/Program.cs
@@ -1,9 +1,16 @@
+using API.Data;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddDbContext<StoreContext>(options => {
+    options.UseSqlite(builder.Configuration
+                             .GetConnectionString("DefaultConnection"));
+});
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
