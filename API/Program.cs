dd22f55500b1a23687102c4a58f60c5bb7791b29
StefanStrauberg <stefanstrauberg@gmail.com>
2023-02-22 23:00:01 +0300

    Services Extensions

--- API/Program.cs
+++ API/Program.cs
@@ -1,16 +1,12 @@
 using System;
-using System.Linq;
-using API.Errors;
+using API.Extensions;
 using API.Helpers;
 using API.Middleware;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,30 +15,11 @@ builder.Services.AddDbContext<StoreContext>(options => {
     options.UseSqlite(builder.Configuration
                              .GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
-builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddAutoMapper(typeof(MappingProfiles));
 builder.Services.AddControllers();
+builder.Services.AddApplicationServices();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.Configure<ApiBehaviorOptions>(options => 
-{
-    options.InvalidModelStateResponseFactory = actionContext =>
-    {
-        var errors = actionContext.ModelState
-                                  .Where(x => x.Value.Errors.Count > 0)
-                                  .SelectMany(x => x.Value.Errors)
-                                  .Select(x => x.ErrorMessage)
-                                  .ToArray();
-
-        var errorResponse = new ApiValidationErrorResponce
-        {
-            Errors = errors
-        };
-
-        return new BadRequestObjectResult(errorResponse);
-    };
-});
 
 var app = builder.Build();
 using(var scope = app.Services.CreateScope())
